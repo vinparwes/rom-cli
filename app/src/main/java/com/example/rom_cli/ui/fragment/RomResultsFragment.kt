cf5f0fb646039fe5bdea48a2619bcb725c1c284b
Vincent <101572886+patriknotvincent@users.noreply.github.com>
2024-04-29 11:17:28 +0200

    Setup for ROM Result screen

---
+++ app/src/main/java/com/example/rom_cli/ui/fragment/RomResultsFragment.kt
@@ -0,0 +1,22 @@
+package com.example.rom_cli.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.rom_cli.databinding.FragmentRomResultsBinding
+
+class RomResultsFragment : Fragment() {
+
+    private var _binding : FragmentRomResultsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentRomResultsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+
+}
